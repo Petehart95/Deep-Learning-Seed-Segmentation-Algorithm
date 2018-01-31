00f89cd8fc5d2da441ecb50e0c1fcffe87c9b71f
Peter Hart <12421031@students.lincoln.ac.uk>
2018-01-31 21:47:08 +0000

    Creating Pre-Trained CNN Model Using Keras

--- Artefact/untitled1.py
+++ Artefact/untitled1.py
@@ -1,34 +1,71 @@
-import os  # handle system path and filenames
-import tensorflow as tf  # import tensorflow as usual
+from keras.models import Sequential
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+import cv2
+import numpy as np
 
-# define a function to list tfrecord files.
-def list_tfrecord_file(file_list):
-    tfrecord_list = []
-    for i in range(len(file_list)):
-        current_file_abs_path = os.path.abspath(file_list[i])		
-        if current_file_abs_path.endswith(".tfrecord"):
-            tfrecord_list.append(current_file_abs_path)
-            print("Found %s successfully!" % file_list[i])				
-        else:
-            pass
-    return tfrecord_list
-	
-# Traverse current directory
-def tfrecord_auto_traversal():
-    current_folder_filename_list = os.listdir("./") # Change this PATH to traverse other directories if you want.
-    if current_folder_filename_list != None:
-        print("%s files were found under current folder. " % len(current_folder_filename_list))
-        print("Please be noted that only files end with '*.tfrecord' will be load!")
-        tfrecord_list = list_tfrecord_file(current_folder_filename_list)
-        if len(tfrecord_list) != 0:
-            for list_index in xrange(len(tfrecord_list)):
-                print(tfrecord_list[list_index])
-        else:
-            print("Cannot find any tfrecord files, please check the path.")
-    return tfrecord_list
+def VGG_16(weights_path=None):
+    model = Sequential()
+    model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(64, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
 
-def main():
-    tfrecord_list = tfrecord_auto_traversal()
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(128, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(256, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Convolution2D(512, 3, 3, activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+    model.add(Flatten())
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(4096, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1000, activation='softmax'))
+
+    if weights_path:
+        model.load_weights(weights_path)
+
+    return model
 
 if __name__ == "__main__":
-    main()
+    im = cv2.resize(cv2.imread('cat.jpg'), (224, 224)).astype(np.float32)
+    im[:,:,0] -= 103.939
+    im[:,:,1] -= 116.779
+    im[:,:,2] -= 123.68
+    im = im.transpose((2,0,1))
+    im = np.expand_dims(im, axis=0)
+
+    # Test pretrained model
+    model = VGG_16('vgg16_weights.h5')
+    sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy')
+    out = model.predict(im)
+    #print np.argmax(out)
